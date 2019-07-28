90bd533b10ba3ad6b49ff99fd37a56d4baf8880e
Tomio Mizoroki <tmizoroki@gmail.com>
2019-07-27 18:49:46 -0700

    Add create a temporary list command
    Add a `list` command that can accept a brawl stars club ID as an
    argument and will send a list of the top 5 club members by current total
    trophy count.

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const Discord = require('discord.js');
+const BrawlStars = require('brawlstars');
+const { TROPHY_BOT_TOKEN, BRAWL_API_TOKEN } = require('./config');
+
 const bot = new Discord.Client();
-const { TROPHY_BOT_TOKEN } = require('./config');
+const brawlStarsClient = new BrawlStars.Client({ token: BRAWL_API_TOKEN });
 
 const TROPHY_BOT_PREFIX = '!trophy';
 
 const commandToHandler = {
-    test(msg, args) {
-        const text = ['Command: test', `Arguments: ${args.join(', ')}`].join('\n');
+    async list(msg, [clubId]) {
+        const clubData = await getClubData(brawlStarsClient, clubId);
+        const tagToMemberData = getTagToMemberData(clubData);
+        const text = Object.values(tagToMemberData).slice(0, 5).map(member => `${member.name}: ${member.trophyCount} trophies`).join('\n')
         msg.channel.send(text);
     }
 }
@@ -17,7 +22,7 @@ bot.on('message', onMessage);
 
 bot.login(TROPHY_BOT_TOKEN);
 
-function onMessage(msg) {
+async function onMessage(msg) {
     if (!getIsTrophyBotRequest(msg)) {
         return;
     }
@@ -32,4 +37,17 @@ function onMessage(msg) {
 
 function getIsTrophyBotRequest(msg) {
     return msg.content.startsWith(TROPHY_BOT_PREFIX);
+}
+
+function getClubData(client, clubId = 'vlyl2q') {
+    return client.getClub(`#${clubId.toUpperCase()}`);
+}
+
+function getTagToMemberData(clubData) {
+    return clubData.members.reduce(toTagToMemberData, {});
+
+    function toTagToMemberData(tagToMemberData, { tag, name, trophies: trophyCount}) {
+        tagToMemberData[tag] = { name, trophyCount };
+        return tagToMemberData;
+    }
 }
