89068445f5db1bb41e6ae2da20e4c86b54844eb4
Tomio Mizoroki <tmizoroki@gmail.com>
2019-07-27 18:11:49 -0700

    Write test bot
    Write a test bot that recognizes a bot command using a prefix, then
    accepts a command called test that will respond with the command name
    and additional provided arguments.

--- index.js
+++ index.js
@@ -1,2 +1,35 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
+const { TROPHY_BOT_TOKEN } = require('./config');
+
+const TROPHY_BOT_PREFIX = '!trophy';
+
+const commandToHandler = {
+    test(msg, args) {
+        const text = ['Command: test', `Arguments: ${args.join(', ')}`].join('\n');
+        msg.channel.send(text);
+    }
+}
+
+bot.on('ready', () => { console.log('Trophy Bot is ready!'); });
+
+bot.on('message', onMessage);
+
+bot.login(TROPHY_BOT_TOKEN);
+
+function onMessage(msg) {
+    if (!getIsTrophyBotRequest(msg)) {
+        return;
+    }
+
+    const [command, ...args] = msg.content.slice(TROPHY_BOT_PREFIX.length)
+                                           .trim()
+                                           .split(' ');
+
+    const handler = commandToHandler[command.toLowerCase()];
+    handler(msg, args);
+}
+
+function getIsTrophyBotRequest(msg) {
+    return msg.content.startsWith(TROPHY_BOT_PREFIX);
+}
