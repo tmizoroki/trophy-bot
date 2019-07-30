4b29a2ac9877f0c531d66dbd5c17c2646066a741
Tomio Mizoroki <tmizoroki@gmail.com>
2019-07-30 01:46:14 -0700

    Firebase service account

--- trophyBotDataService.js
+++ trophyBotDataService.js
@@ -1,15 +1,22 @@
-const firebase = require('firebase');
+const admin = require('firebase-admin');
 
-firebase.initializeApp({
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: "bb-stats-f64b6.firebaseapp.com",
-    databaseURL: "https://bb-stats-f64b6.firebaseio.com",
-    projectId: "bb-stats-f64b6",
-    storageBucket: "",
-    messagingSenderId: "169567647784",
-    appId: "1:169567647784:web:324d1b83ae63aced"
+admin.initializeApp({
+  credential: admin.credential.cert({
+    type: process.env.FIREBASE_TYPE,
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY,
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_AUTH_URI,
+    token_uri: process.env.FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL
+  })
 });
-const firestore = firebase.firestore();
+
+const firestore = admin.firestore();
+
 
 module.exports = {
     saveClubTrophyData,
