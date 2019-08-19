d5b0a7cc9af90c673ec668ed7168e233b27e3119
Tomio Mizoroki <tmizoroki@gmail.com>
2019-08-19 00:15:29 -0700

    Update list command to show top 5 pushers

--- commandHandlerService.js
+++ commandHandlerService.js
@@ -62,27 +62,33 @@ async function link(message, [tag]) {
 
 async function list(msg, [clubId]) {
     const clubData = await brawlStarsDataService.getClubData();
-    const tagToMemberData = brawlStarsDataService.getTagToMemberData(clubData);
-    msg.channel.send({ embed: getListEmbed(tagToMemberData, clubData) });
+
+    const newClubData = await brawlStarsDataService.getClubData();
+    const newTagToMemberData = brawlStarsDataService.getTagToMemberData(newClubData);
+    const todaysTagToMemberData = await readTodaysClubTrophyData(new Date());
+
+    const sortedTrophyPushers = getSortedTrophyPushers(newTagToMemberData, todaysTagToMemberData);
+
+    msg.channel.send({ embed: getListEmbed(sortedTrophyPushers, clubData) });
 }
 
-function getListEmbed(tagToMemberData, clubData, num = 5) {
+function getListEmbed(sortedTrophyPushers, clubData, num = 5) {
     return {
-        title: `Top ${num} ${clubData.name} members`,
-        description: `The top ${num} members of ${clubData.name} by current trophy count.`,
-        fields: getListFields(tagToMemberData),
+        title: `Current Top ${num} Trophy Pushers`,
+        description: `The top ${num} trophy pushers of ${clubData.name} for the current 24 hour period.`,
+        fields: getListFields(sortedTrophyPushers),
         timestamp: new Date(),
     };
 
-    function getListFields(tagToMemberData) {
-        return Object.values(tagToMemberData).slice(0, num)
-                                             .map(toListField);
+    function getListFields(sortedTrophyPushers) {
+        return Object.values(sortedTrophyPushers).slice(0, num)
+                                                 .map(toListField);
     }
 
     function toListField(member, index) {
         return {
-            name: `${index + 1}. ${member.name} - ${member.trophyCount} trophies`,
-            value: 'stuff',
+            name: `${index + 1}. ${member.name}`,
+            value: `${member.trophyDelta} 🏆`,
         };
     }
 }
