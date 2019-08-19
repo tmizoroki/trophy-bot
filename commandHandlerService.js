8f14773c5499c879d77709293670746f3731d53e
Tomio Mizoroki <tmizoroki@gmail.com>
2019-08-19 01:04:32 -0700

    Fix help command white space

--- commandHandlerService.js
+++ commandHandlerService.js
@@ -108,9 +108,9 @@ function getListEmbed(sortedTrophyPushers, clubData, num = 5) {
 
 function help(message) {
     const helpLines = [
-        '!trophy list         show the top 5 trophy pushers for the current 24 hour period',
-        '!trophy link <TAG>   link a brawl stars tag to your discord username',
-        '!trophy rank         show your trophy pusher rank for the current 24 hour period'
+        '!trophy list           show the top 5 trophy pushers for the current 24 hour period',
+        '!trophy link <TAG>     link a brawl stars tag to your discord username',
+        '!trophy rank           show your trophy pusher rank for the current 24 hour period'
     ];
     message.channel.send(helpLines.join('\n'));
 }
