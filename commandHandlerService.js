2c31c4e4aa72ce8ed8985333502a324811ef640d
Tomio Mizoroki <tmizoroki@gmail.com>
2019-09-22 15:50:35 -0700

    Move rank command to separate file

--- commandHandlerService.js
+++ commandHandlerService.js
@@ -1,18 +1,13 @@
-
-const brawlStarsDataService = require('./brawlStarsDataService');
 const {
     updateUsernameToTag,
-    getDocRefId,
-    readClubTrophyData,
-    getMessageAuthorsTag,
     getValidTag,
     getConfig,
     setConfig,
 } = require('./trophyBotDataService');
-const { getSortedTrophyPushers } = require('./trophyUtils');
-const { TROPHY_BOT_PREFIX } = require('./constants');
+
 
 const list = require('./commands/listCommand');
+const rank = require('./commands/rankCommand');
 
 module.exports = {
     list,
@@ -21,52 +16,6 @@ module.exports = {
     config,
     help,
 };
-
-async function rank(message, [tagArg]) {
-    const isTagArgSupplied = !!tagArg;
-    const tag = isTagArgSupplied ? getValidTag(tagArg) : await getMessageAuthorsTag(message);
-    if (!tag) {
-        message.reply(`You have not yet linked your Brawl Stars tag. Please use the command: ${TROPHY_BOT_PREFIX} link <YOUR_BRAWL_STARS_TAG> (without the # in front)`)
-    }
-
-    const currentTagToMemberData = await brawlStarsDataService.getCurrentTagToMemberData();
-    const todaysTagToMemberData = await getTodaysTagToMemberData();
-    const sortedTrophyPushers = getSortedTrophyPushers(currentTagToMemberData, todaysTagToMemberData, 'DESC');
-
-    const index = sortedTrophyPushers.findIndex(pusher => pusher.tag === tag);
-    if (index === -1) {
-        message.reply(`The provided tag: ${tag}, does not match any member tag in this club. If you provided the tag as an argument, check that the tag is correct. Otherwise, try relinking your tag.`);
-    }
-    const rank = sortedTrophyPushers[index].rank;
-    const delta = sortedTrophyPushers[index].trophyDelta;
-    message.reply(`You have pushed ${delta} trophies today (${rank}${getNumSuffix(rank)} place)`);
-}
-
-async function getTodaysTagToMemberData() {
-    let todaysTagToMemberData;
-    try {
-        const todaysDocRefId = getDocRefId(new Date());
-        todaysTagToMemberData = await readClubTrophyData(todaysDocRefId);
-    }
-    catch(error) {
-        const todaysDocRefId = getDocRefId(new Date(), 1)
-        todaysTagToMemberData = await readClubTrophyData(todaysDocRefId);
-    }
-    return todaysTagToMemberData;
-}
-
-function getNumSuffix(rank) {
-    switch(rank) {
-        case 1:
-            return `st`;
-        case 2:
-            return 'nd';
-        case 3:
-            return 'rd';
-        default:
-            return 'th';
-    }
-}
 
 async function link(message, [tag]) {
     if (!tag) {
